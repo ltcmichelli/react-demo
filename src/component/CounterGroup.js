8700840b9521992de3d87f81cb7d6823f10efe08
ltcmichelli <42404448+ltcmichelli@users.noreply.github.com>
2020-04-21 16:03:03 +0800

    [Michelle] Create CounterGroup, move Counter into CounterGroup

---
+++ src/component/CounterGroup.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import Counter from './Counter';
+
+
+class CounterGroup extends Component {
+    render() {
+        return (
+            <div>
+                <Counter />
+            </div>
+        );
+    }
+}
+
+CounterGroup.propTypes = {
+
+};
+
+export default CounterGroup;
